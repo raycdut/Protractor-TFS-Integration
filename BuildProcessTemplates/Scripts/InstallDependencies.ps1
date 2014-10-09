b4f6a15ce8feab23b39e87bf61dda3f050c82e4a
Daniel Weber <daniel.weber@zuehlke.com>
2014-10-09 15:03:58 +0200

    integrated hack for failing webdriver-manager update command

--- BuildProcessTemplates/Scripts/InstallDependencies.ps1
+++ BuildProcessTemplates/Scripts/InstallDependencies.ps1
@@ -13,7 +13,30 @@ else
 	Write-Host "Missing dependencies - installing protractor and webdriver manager"
     npm install protractor -g
     npm install webdriver-manager -g
-	Invoke-Expression ("$webdriverManager update")
+	# The following code is a workaround for >Invoke-Expression "$webdriverManager update"<
+    # At the moment the update get the files but everything is corrupt. Therefore we download it manually.
+    # As soon the update is fixed this can be replaced.
+    $urlChromeDriver = "https://chromedriver.storage.googleapis.com/2.10/chromedriver_win32.zip"
+    $urlSelenium = "http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar"
+    
+    $seleniumFolder = $env:APPDATA + "\npm\node_modules\webdriver-manager\selenium"
+
+    $destinationChromeDriver = $seleniumFolder + "\chromedriver_2.10.zip"
+    $destintaionSelenium = $seleniumFolder +"\selenium-server-standalone-2.42.2.jar"
+    
+    New-Item -ItemType directory -Path $seleniumFolder
+
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($urlChromeDriver, $destinationChromeDriver)
+    $webClient.DownloadFile($urlSelenium, $destintaionSelenium)
+
+	# unzip the chrome driver
+	$shell_app = New-Object -com shell.application
+	cd $seleniumFolder
+	
+	$zipFile = $shell_app.namespace((Get-Location).Path + "\chromedriver_2.10.zip")
+	$destination = $shell_app.namespace((Get-Location).Path)
+	$destination.Copyhere($zipFile.items())
 }
 
 $configFolder = split-path $protractorConfig
